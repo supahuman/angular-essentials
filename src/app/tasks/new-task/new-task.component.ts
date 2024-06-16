c4c25290481c7ce07865caad87ca976ff297a84d
juaji <supahuman>
2024-06-16 17:40:22 -0500

    add new-task component and rendering conditionally

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { Task } from '../task/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {}
