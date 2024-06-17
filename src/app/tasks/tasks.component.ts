5c18a04a01eb38532f033173345171594cb9be2b
juaji <supahuman>
2024-06-17 16:55:42 -0500

    refactor complete button to use tasks service to remove item

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksService } from './tasks.service';
-import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -15,17 +14,12 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
 
-  //Rendering conditionally
   isAddingTask = false;
 
   constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
-  }
-
-  onCompleteTask(id: string) {
-    this.tasksService.removeTask(id);
   }
 
   onStartAddTask() {
